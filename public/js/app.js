42a9b1a0ff2a73edd599242397d967f1f994ef8e
praveen <praveen.arunachalam74@gmail.com>
2016-08-16 07:40:30 +0530

    Add form for message submit

--- public/js/app.js
+++ public/js/app.js
@@ -7,3 +7,17 @@ socket.on('connect', function() {
 socket.on('message', function(message) {
   console.log('message', message.text);
 });
+
+// Handles submitting of new message
+var $form = jQuery('#message-form');
+
+$form.on('submit', function(event) {
+  event.preventDefault();
+  var $message = $form.find('input[name="message"]');
+
+  socket.emit('message', {
+    text: $message.val()
+  });
+  $message.val('');
+  $message.focus();
+});
